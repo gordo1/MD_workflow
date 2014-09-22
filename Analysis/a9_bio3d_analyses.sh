23071117455f474a58600661f0a0377326865674
gordo1 <se2gordon@students.latrobe.edu.au>
2014-09-22 11:39:17 +1000

    Non-functional, so removed

--- Analysis/a9_bio3d_analyses.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# AUTHOR:   Shane Gordon
-# FILE:     a5_bio3d_analyses.sh
-# ROLE:     TODO (some explanation)
-# CREATED:  2014-05-31 17:36:39
-# MODIFIED: 2014-05-31 17:36:59
-
-R CMD BATCH ../Scripts/Analysis_Scripts/a5_bio3d_analyses.R
