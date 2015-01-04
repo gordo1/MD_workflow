bf8ab16269502e58b74f5425560333a4ac7c9b1e
gordo1 <se2gordon@students.latrobe.edu.au>
2015-01-04 17:38:51 +1100

    feed in args from the command line instead of having to dig into the script

--- BUILD_DIR/01-autopsf.tcl
+++ BUILD_DIR/01-autopsf.tcl
@@ -1,31 +1,39 @@
 #!/usr/bin/env tclsh
-#################################################################################
-#   
-#   File Name: 01-autopsf.tcl
-#   Created: Thu 22 May 2014 14:21:19 EST
-#   Last Modified: Thu 22 May 2014 14:47:13 EST
-#   Created By: Shane Gordon
-#
-################################################################################
+# AUTHOR:   Shane Gordon
+# FILE:     01-autopsf.tcl
+# ROLE:     TODO (some explanation)
+# CREATED:  2015-05-22 14:21:19
+# MODIFIED: 2015-01-04 17:37:52
 
-# Loads PDB molecule from the Protein Data Base
+# Instructions --------------------------------------------------------------- {{{
 
-set input "change_me"
-set output "change_me"
+# Call this script using an external shell command:
+#   vmd -e 01-autops.tcl -args "PDB_input_prefix"
+#   Replacing "PDB_input_prefix" with an appropriate value
+#   Be sure to feed in only one arg!
+
+# }}}
+
+# Load PDB molecule from the Protein Data Base ------------------------------- {{{
+
+set input "$argv"
+set output "${input}.autopsf"
 set temp_prot "temp_${input}"
 mol load pdb ${input}.pdb
 
-#   Creates protein-only and lipid-only PDBs for segment building
-################################################################################
+#  }}}
+
+# Creates protein-only selection for segment building ------------------------ {{{
 
 #   Protein
 set sel [atomselect top "protein and noh" frame 0]
 $sel writepdb $temp_prot.pdb
 
-#   PSFGEN part
-################################################################################
+#  }}}
 
-# Renames isoleucine delta-carbon atom from CD1 to CD, as specified in 
+# PSFGEN part ---------------------------------------------------------------- {{{
+
+# Renames isoleucine delta-carbon atom from CD1 to CD, as specified in
 # topology file
 package require psfgen
 topology ../InputFiles/Parameters/top_all27_prot_lipid.rtf
@@ -42,3 +50,5 @@ resetpsf
 file delete ${temp_prot}.pdb
 file delete ${temp_prot}.psf
 exit
+
+# }}}
