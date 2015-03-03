c1e4ec8aec5557f9c1c7479f0b6112a663c0cc55
Shane Gordon <se2gordon@students.latrobe.edu.au>
2015-03-03 21:19:55 +1100

    secondary structure plot changed to stacked line plot. easier to interpret IMHO

--- Scripts/Analysis_Scripts/a5_other_analyses.sh
+++ Scripts/Analysis_Scripts/a5_other_analyses.sh
@@ -3,24 +3,27 @@
 # FILE:     a5_other_analyses.sh
 # ROLE:     TODO (some explanation)
 # CREATED:  2014-06-03 22:02:52
-# MODIFIED: 2014-09-22 11:22:33
+# MODIFIED: 2015-03-03 21:13:18
 
 vmd -dispdev text -e ../Scripts/Analysis_Scripts/a5_other_analyses.tcl
+
+# Concatenate secondary structures
+
+if [[ -d ./SecondaryStructure ]]; then
+  paste ./SecondaryStructure/* > ./SecondaryStructure/SecondaryStructure.dat
+fi
 
 # Some useful plots
 
 GNUPLOTTEMPLATE="../Scripts/Gnuplot_Scripts/template_4plot.gpi"
 gnuplot -e "OUTPUT='./analysis.pdf'" \
-        -e "FILE1p1='./SecondaryStructure/helixPercent.plt'" \
-        -e "FILE1p2='./SecondaryStructure/betaPercent.plt'" \
-        -e "FILE1p3='./SecondaryStructure/turnPercent.plt'" \
-        -e "FILE1p4='./SecondaryStructure/coilPercent.plt'" \
+        -e "SECONDARYSTRUCTURE='./SecondaryStructure/SecondaryStructure.dat'" \
         -e "YLABEL1='Percent Structure'" \
         -e "XLABEL1='Frame (10 ps frame^{-1})'" \
-        -e "TITLE1p1='Helix'" \
-        -e "TITLE1p2='Beta'" \
-        -e "TITLE1p3='Turn'" \
-        -e "TITLE1p4='Coil'" \
+        -e "TITLE1p1='Beta'" \
+        -e "TITLE1p2='Coil'" \
+        -e "TITLE1p3='Helix'" \
+        -e "TITLE1p4='Turn'" \
         -e "FILE2='./protein_radius_gyration.txt'" \
         -e "YLABEL2='R_{g} (Angstrom)'" \
         -e "XLABEL2='Frame (10 ps frame^{-1})'" \
