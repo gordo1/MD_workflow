81d366f46a8b19d0088a02d3b35b7f739ec0a8ea
Shane Gordon <se2gordon@students.latrobe.edu.au>
2015-06-27 20:58:12 +1000

    don't need this

--- Analysis/a4_ligand_cluster_analysis.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# AUTHOR:   Shane Gordon
-# FILE:     a4_ligand_cluster_analysis.sh
-# ROLE:     TODO
-# CREATED:  2014-06-03 13:32:45
-# MODIFIED: 2014-10-27 22:48:07
-
-vmd -dispdev text -e ../Scripts/Analysis_Scripts/a4_ligand_cluster_analysis.tcl
